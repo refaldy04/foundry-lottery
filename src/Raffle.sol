1b80a5667a11847672de8f9522832fe083bb261c
refaldy <refaldybanggana@gmail.com>
2025-02-10 05:26:50 +0700

    setup

---
+++ src/Raffle.sol
@@ -0,0 +1,49 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+ * @title A sample Raffle contract
+ * @author Refaldy Bagas Anggana
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+    /**
+     * Getter Functions
+     */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
